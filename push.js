b0637116913bddc883d8db72ce09398a4dda84ee
Pramodh Bettadapura <pramodhbm@outlook.com>
2025-11-26 21:31:33 +0530

    4

--- push.js
+++ push.js
@@ -11,6 +11,15 @@ push.setVapidDetails(
   keys.privateKey
 );
 
-let sub = {"endpoint":"https://web.push.apple.com/QAP3PI1AdPXWRAkXgpZ3_62LNDrTBhtRvMQ8aWNEnAZjRGU3Tfewmjix4VS2CzzeX4tMj3-qbZYPVny0gO6QkL8KbTggAISvZkLHZhmJYyo7G9yAscBBGMPkLaW11uk0kcH36mmFrRY6nAeSOg41bIbDRktzoGinJkIRQ6koaeU","keys":{"p256dh":"BGK0EvDiFARx6u86ASXDPgG8WfX29SewopQkcP7UCNOeBcnxn5ds_G-ZX665xA6Zxpkg40wFz7T93mtvwQ2a_bI","auth":"PCUZo2YqRnWw6-6jh5L0vA"}}
+let sub = {
+  "endpoint": "https://web.push.apple.com/QAP3PI1AdPXWRAkXgpZ3_62LNDrTBhtRvMQ8aWNEnAZjRGU3Tfewmjix4VS2CzzeX4tMj3-qbZYPVny0gO6QkL8KbTggAISvZkLHZhmJYyo7G9yAscBBGMPkLaW11uk0kcH36mmFrRY6nAeSOg41bIbDRktzoGinJkIRQ6koaeU",
+  "keys": {
+    "p256dh": "BGK0EvDiFARx6u86ASXDPgG8WfX29SewopQkcP7UCNOeBcnxn5ds_G-ZX665xA6Zxpkg40wFz7T93mtvwQ2a_bI",
+    "auth": "PCUZo2YqRnWw6-6jh5L0vA" 
+  } 
+}
 
-push.sendNotification(sub, "Your push message here");
+let sub2 = {"endpoint":"https://web.push.apple.com/QAdY4SSnGoEYhXnrK0dTqUoViJ7dzYeSBQMHu8_jczZKvgVgP67TmTG0_XkFHHimGMGVSUyxJKy4Qu5e98vtGv9HppIE5--JFa7WwDcIPmtjfHVoMDZ8r3-RyFF57rl_1flEaOcUVgFsBjKIPR2JRxCFGzPV0xRrnrKaKJjhfoU","keys":{"p256dh":"BM3JYU0UF198zC0RmCb6JiuhvhePsRrRcYsY794QATFkld2wyOk5PjrjdNokF3ymQK_HbYrQCllj2-Gfi-nNaS8","auth":"PD8Rl2R5wVIYsAtKBpiOAw"}}
+
+push.sendNotification(sub, "Your push message here 2");
+push.sendNotification(sub2, "Hey this is working too!");
--- sw.js
+++ sw.js
@@ -1,8 +1,20 @@
 self.addEventListener('push', event => {
-  const data = event.data ? event.data.text() : 'No payload';
+
+  var options = {
+    body: 'This is a push notification.',
+    vibrate: [100, 50, 100],
+    data: {
+      dateOfArrival: Date.now(),
+      primaryKey: 1
+    },
+    actions: [
+      {action: 'explore', title: 'Explore this new world',
+        icon: 'images/checkmark.png'},
+      {action: 'close', title: 'Close',
+        icon: 'images/xmark.png'},
+    ]
+  };
   event.waitUntil(
-    self.registration.showNotification('Push Notification', {
-      body: data,
-    })
+    self.registration.showNotification('Push Notification', options)
   );
 });
